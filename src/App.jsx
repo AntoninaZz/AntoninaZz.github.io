41c976e8353eb81e9d888c7da2a0447f268962a8
AntoninaZz <aazdebska.fitu19@kubg.edu.ua>
2025-02-20 16:38:30 +0200

    add contacts component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // import { useState } from 'react'
 import './App.css'
+import data from '../data.json'
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -15,11 +16,19 @@ function MainInfo() {
   return (
     <>
       <div>
-        <img src={"/profile-photo.jpg"} alt="Antonina Zdebska profile photo" width={50}/>
+        <img src="/profile-photo.jpg" alt="Antonina Zdebska profile photo" width={100} />
         <p>Antonina Zdebska</p>
+        <Contact type="linkedin" />
+        <Contact type="instagram" />
+        <Contact type="github" />
+        <Contact type="facebook" />
       </div>
     </>
   );
+}
+
+function Contact({ type }) {
+  return (<a href={data.contacts[type]} target="_blank"><img src={type + ".svg"} alt={type} /></a>);
 }
 
 export default App
