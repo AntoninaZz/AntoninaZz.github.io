8771b304a7b620195cf0cd7c07bebe561fd06c40
AntoninaZz <aazdebska.fitu19@kubg.edu.ua>
2025-02-20 17:58:21 +0200

    add education section

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,25 @@ function App() {
     <>
       <button onClick={() => setLang(lang == 'ua' ? 'en' : 'ua')}>{lang == 'ua' ? "🇬🇧" : "🇺🇦"}</button>
       <MainInfo language={lang} />
+      <section>
+        <h2>{lang == 'ua' ? "про себе" : "about"}</h2>
+        <p>{data.profile.about[lang]}</p>
+      </section>
+      <section>
+        <h2>{lang == 'ua' ? "освіта" : "education"}</h2>
+        <img src="/education-photo.png" alt="Antonina Zdebska Masters degree photo" width={100} />
+        {data.education.map((ed) => (<Education info={ed} lang={lang} />))}
+      </section>
     </>
   )
 }
 
 function MainInfo({ language }) {
-  console.log(language)
   return (
     <>
       <div>
         <img src="/profile-photo.jpg" alt="Antonina Zdebska profile photo" width={100} />
-        <p>{data.profile.name[language]}</p>
+        <h1>{data.profile.name[language]}</h1>
         <Contact type="linkedin" />
         <Contact type="github" />
         <Contact type="instagram" />
@@ -31,6 +39,16 @@ function MainInfo({ language }) {
 
 function Contact({ type }) {
   return (<a href={data.contacts[type]} target="_blank"><img src={type + ".svg"} alt={type} /></a>);
+}
+
+function Education({ info, lang }) {
+  return (
+    <>
+      <h3>{info.school[lang]}</h3>
+      <h4>{info.degree[lang]}</h4>
+      <p>{info.years[lang]}</p>
+    </>
+  );
 }
 
 export default App
