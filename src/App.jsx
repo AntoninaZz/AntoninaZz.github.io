dee985449c73744588a1b07cf4025edd6de464c8
AntoninaZz <aazdebska.fitu19@kubg.edu.ua>
2025-02-21 12:52:52 +0200

    added skills

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,10 @@ function App() {
         <img src="/education-photo.png" alt="Antonina Zdebska Masters degree photo" width={100} />
         {data.education.map((ed) => (<Education info={ed} lang={lang} />))}
       </section>
+      <section>
+        <div>{data.skills.soft.map((skill) => (<SoftSkill info={skill} lang={lang} />))}</div>
+        <div>{data.skills.hard.map((skill) => (<HardSkill info={skill} />))}</div>
+      </section>
     </>
   )
 }
@@ -28,6 +32,7 @@ function MainInfo({ language }) {
       <div>
         <img src="/profile-photo.jpg" alt="Antonina Zdebska profile photo" width={100} />
         <h1>{data.profile.name[language]}</h1>
+        <p>{data.profile.position[language]}</p>
         <Contact type="linkedin" />
         <Contact type="github" />
         <Contact type="instagram" />
@@ -47,6 +52,23 @@ function Education({ info, lang }) {
       <h3>{info.school[lang]}</h3>
       <h4>{info.degree[lang]}</h4>
       <p>{info.years[lang]}</p>
+    </>
+  );
+}
+
+function HardSkill({ info }) {
+  return (
+    <div style={{display: "inline-block", width: "15%", padding: 5}}>
+    <div>{info.skill}</div>
+    <div style={{width: info.level*20+'%', height: 2, backgroundColor: "blueviolet"}}></div>
+    </div>
+  );
+}
+
+function SoftSkill({ info, lang }) {
+  return (
+    <>
+    <span style={{padding: 5}}>{info[lang]}</span>
     </>
   );
 }
