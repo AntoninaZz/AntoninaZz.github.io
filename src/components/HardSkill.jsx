e12d3805d8399d5bce7b6f8b11844228688cac6b
AntoninaZz <zdanton02@gmail.com>
2025-02-26 11:51:34 +0200

    Hardskill styles done

--- src/components/HardSkill.jsx
+++ src/components/HardSkill.jsx
@@ -5,8 +5,8 @@ import '../css/colors.css';
 function HardSkill({ info }) {
     return (
         <div className='hard skill'>
-            <div className='title'>{info.skill}</div>
-            <div className='level' style={{ width: info.level * 20 + '%', height: 2}} ></div>
+            <div className='title'>{info.skill}<div className='level' style={{ width: info.level * 20 + '%', height: 2 }} ></div></div>
+            <div className='level' style={{ width: info.level * 20 + '%', height: 2 }} ></div>
         </div>
     );
 }
